1f73511eeaac1189cedf28a208fa5232fecab42e
aldanciprian <aldanciprian@gmail.com>
2018-10-08 16:49:09 +0300

    more apartments

--- filter.sh
+++ filter.sh
@@ -1,1 +1,10 @@
-mysql -u ciprian -D imobiliare -e "select link from olx;" | grep -iv "giroc\|sag\|dambovita\|freidorf\|fraidorf\|iosefin\|mansarda\|dumbravita\|braytim\|link\|braitym"
+#!/bin/sh
+
+
+
+OUT=$(mysql -u ciprian -D imobiliare -e "select link from olx;" | grep -iv "giroc\|sag\|dambovita\|freidorf\|fraidorf\|iosefin\|mansarda\|dumbravita\|braytim\|link\|braitym\|steaua\|mosnita\|mu[sz]icescu\|soarelui\|buziasului\|fabric\|[dD]ambovita")
+
+
+OUT_FILT=$( echo "$OUT" | awk -F"#" '{print $1}' | sort  | uniq )
+
+echo "$OUT_FILT"
